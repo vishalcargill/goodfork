dcd674a1634e76940e51ebb00e0880ada616e7e4
SamikshaKumari <samiksha_kumari@cargill.com>
2025-12-01 13:08:38 +0530

    fix issue

--- src/app/api/admin/recipes/route.ts
+++ src/app/api/admin/recipes/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function POST(request) {
+export async function POST(request: Request) {
   return NextResponse.json(
     {
       success: false,
